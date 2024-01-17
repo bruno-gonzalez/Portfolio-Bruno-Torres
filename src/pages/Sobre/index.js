bde15f7df479e87906c07c441b3f1900a14d6f9f
Bruno Gonzalez <89939064+bruno-gonzalez@users.noreply.github.com>
2024-01-17 11:58:21 -0300

    Update sobre mim

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -8,13 +8,7 @@ export default function Sobre() {
             <img className={styles.foto} src='https://github.com/bruno-gonzalez.png' alt='Minha foto' />
             <div className={styles.container__text}>
                <p className={styles.text}>
-                  Olá, eu sou o Bruno, desenvolvedor Front-end. 
-                  Tenho 20 anos e faço graduação de Sistemas da Informação na Descomplica Faculdade Digital. 
-                  Tenho conhecimento em CSS, JavaScript, React e Typescript, 
-                  minha meta é ser programador Full stack mas atualmente tenho conhecimento apenas no front-end. 
-                  Há 1 ano e meio, concluo diversos cursos de front-end na Alura.
-                  Sou apaixonado pelo desenvolvimento de componentes reutilizáveis.
-                  Busco minha primeira oportunidade na área de tecnologia.
+                  Meu nome é Bruno, sou desenvolvedor Front-end. Tenho 21 anos e faço graduação de Sistemas da Informação na Descomplica Faculdade Digital. Tenho conhecimento em CSS, JavaScript, C#, React e Typescript. Há 3 anos, concluo diversos cursos na Alura. 
                </p>
             </div>
          </div>
